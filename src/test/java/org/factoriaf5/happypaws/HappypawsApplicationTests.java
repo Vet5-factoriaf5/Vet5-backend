cb28bbb23c2e87d7fa5062b4453e5f2d3ec312c8
saulOMgit <sauloteromelchor@gmail.com>
2025-09-15 09:26:13 +0200

    add First Structure

---
+++ src/test/java/org/factoriaf5/happypaws/HappypawsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.happypaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappypawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
