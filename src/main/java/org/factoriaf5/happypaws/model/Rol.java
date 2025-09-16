e9b589c9c2d581934a3595c61c08c471c91f2e01
¨Saul¨ <sauloteromelchor@gmail.com>
2025-09-16 14:56:26 +0200

    modify dependencies, add User & Rol model

---
+++ src/main/java/org/factoriaf5/happypaws/model/Rol.java
@@ -0,0 +1,21 @@
+package org.factoriaf5.happypaws.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre; // Ejemplo: ADMIN, CLIENTE
+}
---
+++ src/main/java/org/factoriaf5/happypaws/model/Usuario.java
@@ -0,0 +1,40 @@
+package org.factoriaf5.happypaws.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username; // Puede ser email o DNI
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "usuarios_roles",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "rol_id")
+    )
+    @Builder.Default
+    private Set<Rol> roles = new HashSet<>();
+}
