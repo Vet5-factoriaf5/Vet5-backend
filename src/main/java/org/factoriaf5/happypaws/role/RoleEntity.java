5515ecfbd427572aecc2e194d5e5d410519563a1
saulOMgit <sauloteromelchor@gmail.com>
2025-09-24 15:18:31 +0200

    add User & Rol

---
+++ src/main/java/org/factoriaf5/happypaws/role/RoleEntity.java
@@ -0,0 +1,31 @@
+package org.factoriaf5.happypaws.role;
+
+import java.util.Set;
+
+import org.factoriaf5.happypaws.user.UserEntity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class RoleEntity {
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id_role")
+    private Long id;
+    private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserEntity> users;
+
+}
