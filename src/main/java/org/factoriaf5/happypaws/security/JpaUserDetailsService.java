6268344ccce492926badaa6876f34aafdc8f9bdf
¨Saul¨ <sauloteromelchor@gmail.com>
2025-09-25 08:50:56 +0200

    add Folder Security

---
+++ src/main/java/org/factoriaf5/happypaws/security/JpaUserDetailsService.java
@@ -0,0 +1,36 @@
+package org.factoriaf5.happypaws.security;
+
+import java.util.Optional;
+
+import org.factoriaf5.happypaws.user.UserEntity;
+import org.factoriaf5.happypaws.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public JpaUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // Optional<UserEntity> user = userRepository.findByUsername(username);
+
+        // System.out.println("<----- " + user.get().getUsername());
+        // System.out.println("<----- " + user.get().getPassword());
+        // System.out.println("<----- " + user.get().getRoles().toString());
+
+        return userRepository.findByUsername(username)
+                .map(SecurityUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+    }
+
+}
