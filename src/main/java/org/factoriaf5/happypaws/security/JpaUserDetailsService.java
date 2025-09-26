24dc4477747b3a75f22f7cde6ba9b156ebf15dc2
saulOMgit <sauloteromelchor@gmail.com>
2025-09-26 13:58:44 +0200

    fix Giaco + test admin login

--- src/main/java/org/factoriaf5/happypaws/security/JpaUserDetailsService.java
+++ src/main/java/org/factoriaf5/happypaws/security/JpaUserDetailsService.java
@@ -1,12 +1,5 @@
 package org.factoriaf5.happypaws.security;
 
-import java.util.Optional;
-
-import org.factoriaf5.happypaws.user.UserEntity;
-
-//import java.util.Optional;
-
-//import org.factoriaf5.happypaws.user.UserEntity;
 import org.factoriaf5.happypaws.user.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,12 +17,6 @@ public class JpaUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        Optional<UserEntity> user = userRepository.findByUsername(username);
-
-        System.out.println("<----- " + user.get().getUsername());
-        System.out.println("<----- " + user.get().getPassword());
-        System.out.println("<----- " + user.get().getRoles().toString());
 
         return userRepository.findByUsername(username)
                 .map(SecurityUser::new)
